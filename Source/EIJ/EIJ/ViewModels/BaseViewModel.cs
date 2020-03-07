a4579c877af3e02180570e9e958a29bfc40ea4c8
Phillip Smith <FilupErwinSmith@gmail.com>
2020-03-07 15:39:19 +1100

    Initial project push

---
+++ Source/EIJ/EIJ/ViewModels/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using EIJ.Models;
+using EIJ.Services;
+using EIJ.Types;
+using Xamarin.Forms;
+
+namespace EIJ.ViewModels
+{
+  public class BaseViewModel : PropertyChangedBase
+  {
+    private bool isBusy;
+
+    private string title = string.Empty;
+    public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
+
+    public bool IsBusy
+    {
+      get => isBusy;
+      set => SetValue(ref isBusy, value);
+    }
+
+    public string Title
+    {
+      get => title;
+      set => SetValue(ref title, value);
+    }
+  }
+}
---
+++ Source/EIJ/EIJ/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using EIJ.Models;
+
+namespace EIJ.ViewModels
+{
+  public class ItemDetailViewModel : BaseViewModel
+  {
+    public Item Item { get; set; }
+    public ItemDetailViewModel(Item item = null)
+    {
+      Title = item?.Text;
+      Item = item;
+    }
+  }
+}
