a94d6e63141457484867a376f9891a467c3594f3
Phillip Smith <FilupErwinSmith@gmail.com>
2020-03-08 00:22:46 +1100

    Renamed About to Rules

---
+++ Source/EIJ/EIJ/ViewModels/RulesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace EIJ.ViewModels
+{
+  public class RulesViewModel : BaseViewModel
+  {
+    public ICommand OpenWebCommand { get; }
+
+    public RulesViewModel()
+    {
+      Title = "Rules";
+      OpenWebCommand = new Command(async () => await Browser.OpenAsync(@"https://rulebook.io/games/everyone-is-john/rules/"));
+    }
+  }
+}
--- Source/EIJ/EIJ/Views/RulesPage.xaml.cs
+++ Source/EIJ/EIJ/Views/RulesPage.xaml.cs
@@ -8,9 +8,9 @@ namespace EIJ.Views
   // Learn more about making custom code visible in the Xamarin.Forms previewer
   // by visiting https://aka.ms/xamarinforms-previewer
   [DesignTimeVisible(false)]
-  public partial class AboutPage : ContentPage
+  public partial class RulesPage : ContentPage
   {
-    public AboutPage()
+    public RulesPage()
     {
       InitializeComponent();
     }
