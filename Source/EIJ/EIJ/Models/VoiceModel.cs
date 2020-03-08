4c48b3dc6517cc3fe2b431f1d3d741665d29c73f
Phillip Smith <FilupErwinSmith@gmail.com>
2020-03-08 16:49:07 +1100

    Added VoicesModel

---
+++ Source/EIJ/EIJ/Models/VoiceModel.cs
@@ -0,0 +1,15 @@
+﻿using EIJ.Types;
+
+namespace EIJ.Models
+{
+  internal class VoiceModel : PropertyChangedBase
+  {
+    private string _playerName;
+
+    public string PlayerName
+    {
+      get => _playerName;
+      set => SetValue(ref _playerName, value);
+    }
+  }
+}
