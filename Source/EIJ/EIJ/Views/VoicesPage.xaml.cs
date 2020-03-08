d802abd44b97f582e908fed9ea1074b6e6a6286b
Phillip Smith <FilupErwinSmith@gmail.com>
2020-03-08 16:48:44 +1100

    Added VoicesPage

---
+++ Source/EIJ/EIJ/Views/VoicesPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EIJ.Views
+{
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class VoicesPage : ContentPage
+  {
+    public VoicesPage()
+    {
+      InitializeComponent();
+    }
+  }
+}
