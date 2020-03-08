7a5f8ace64b0786302dd070f3bc1f23ef93fee97
Phillip Smith <FilupErwinSmith@gmail.com>
2020-03-08 16:47:22 +1100

    Added extension method

---
+++ Source/EIJ/EIJ/Extensions/ObservableCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace EIJ.Extensions
+{
+  internal static class ObservableCollectionExtensions
+  {
+    public static void AddRange<T>(this ObservableCollection<T> thisCollection, IEnumerable<T> itemsToAdd)
+    {
+      foreach (var item in itemsToAdd)
+      {
+        thisCollection.Add(item);
+      }
+    }
+  }
+}
